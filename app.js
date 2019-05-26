6cbe426699b292ac5cc231f01f4fb8ef213a14df
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-05-25 17:18:12 -0700

    update port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 const leaderboard = {};
 
 const userIdGenerator = (function* () {
